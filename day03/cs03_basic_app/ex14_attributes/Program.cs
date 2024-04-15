3bcd7cabc2bc44a661a83b48461b4fd8f8ac4a59
Geuna00 <rmsdk617@naver.com>
2024-04-15 12:10:25 +0900

    up

---
+++ day03/cs03_basic_app/ex14_attributes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+
+namespace ex14_attributes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("리플렉션!");
+
+            int a = int.MaxValue;
+            Type type = a.GetType();
+            Console.WriteLine(type.FullName); // System.Int32
+
+            float f = float.MaxValue;
+            Console.WriteLine(f.GetType()); // System.Single
+
+            double d = double.MaxValue;
+            Console.WriteLine(d.GetType()); // System.Double
+
+            FieldInfo[] fields = type.GetFields(); // 타입 객체에서 어떤 필드가 있는지 모두 확인
+            foreach (var item in fields)
+            {
+                Console.WriteLine($"Type : {item.FieldType}, Name : {item.Name}");
+            }
+
+           
+        }
+    }
+}
