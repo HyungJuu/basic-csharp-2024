e4f1c6d8b9c5d4f5e8fdec782e4742ac4e016033
Geuna00 <rmsdk617@naver.com>
2024-04-15 12:32:33 +0900

    up

--- day03/cs03_basic_app/ex14_attributes/Program.cs
+++ day03/cs03_basic_app/ex14_attributes/Program.cs
@@ -2,11 +2,33 @@
 
 namespace ex14_attributes
 {
+    class MyClass
+    {
+        [Obsolete("이 메서드는 다음버전에서 폐기됩니다. NewMethod()를 사용하세요.", false)]  // ture는 아예 사용불가
+        /// <summary>
+        /// 올드메서드(이렇게 쓰세요)
+        /// </summary>
+
+        public void OldMethod() // 최초에 제작 메서드
+        {
+            Console.WriteLine("Old Method!");
+        }
+
+        /// <summary>
+        /// 뉴메서드.(제발 이렇게 쓰세요)
+        /// </summary>
+        public void NewMethod() // 개선한 메서드
+        {
+            Console.WriteLine("New Method!");
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("리플렉션!");
+            #region '리플렉션'
+            Console.WriteLine("리플렉션!"); // GetType(); 만 알고 있으면됨
 
             int a = int.MaxValue;
             Type type = a.GetType();
@@ -17,14 +39,29 @@ namespace ex14_attributes
 
             double d = double.MaxValue;
             Console.WriteLine(d.GetType()); // System.Double
+            Console.WriteLine();
 
+            // Advanced 개발시에 필요한 내용
             FieldInfo[] fields = type.GetFields(); // 타입 객체에서 어떤 필드가 있는지 모두 확인
             foreach (var item in fields)
             {
                 Console.WriteLine($"Type : {item.FieldType}, Name : {item.Name}");
             }
 
-           
+            MethodInfo[] methods = type.GetMethods(); // 타입 객체에서 어떤 필드가 있는지 모두 확인
+            foreach (var item in methods)
+            {
+                Console.WriteLine($"Type : {item.DeclaringType}, Name : {item.Name}");
+            }
+            Console.WriteLine();
+            #endregion
+
+            // 애트리뷰트
+            Console.WriteLine("애트리뷰트!");
+
+            MyClass myClass = new MyClass();
+            myClass.OldMethod();
+            myClass.NewMethod();
         }
     }
 }
