f5e44e1ee771b5a9030946f4ad513e532ede904e
Geuna00 <rmsdk617@naver.com>
2024-04-16 17:21:59 +0900

    20240416 upload

--- day04/cs04_winform_app/ex18_winControlApp/FrmMain.Designer.cs
+++ day04/cs04_winform_app/ex18_winControlApp/FrmMain.Designer.cs
@@ -28,13 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
             groupBox1 = new GroupBox();
             TxtSampleText = new TextBox();
             ChkItalic = new CheckBox();
             ChkBold = new CheckBox();
             CboFonts = new ComboBox();
             label1 = new Label();
+            groupBox2 = new GroupBox();
+            PrgDummy = new ProgressBar();
+            TrbDummy = new TrackBar();
+            groupBox3 = new GroupBox();
+            BtnQuestion = new Button();
+            BtnMsgBox = new Button();
+            BtnModaless = new Button();
+            BtnModal = new Button();
+            groupBox4 = new GroupBox();
+            BtnAddChild = new Button();
+            BtnAddRoot = new Button();
+            LsvDummy = new ListView();
+            TrvDummy = new TreeView();
+            groupBox5 = new GroupBox();
+            BtnLoad = new Button();
+            PicNormal = new PictureBox();
+            DlgOpenImage = new OpenFileDialog();
             groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)TrbDummy).BeginInit();
+            groupBox3.SuspendLayout();
+            groupBox4.SuspendLayout();
+            groupBox5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)PicNormal).BeginInit();
             SuspendLayout();
             // 
             // groupBox1
@@ -47,7 +71,7 @@
             groupBox1.Font = new Font("나눔고딕", 8.999999F);
             groupBox1.Location = new Point(12, 12);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(455, 236);
+            groupBox1.Size = new Size(436, 123);
             groupBox1.TabIndex = 0;
             groupBox1.TabStop = false;
             groupBox1.Text = "콤보박스, 체크박스, 텍스트박스";
@@ -55,9 +79,9 @@
             // TxtSampleText
             // 
             TxtSampleText.Font = new Font("나눔고딕", 8.999999F);
-            TxtSampleText.Location = new Point(34, 79);
+            TxtSampleText.Location = new Point(23, 79);
             TxtSampleText.Name = "TxtSampleText";
-            TxtSampleText.Size = new Size(392, 21);
+            TxtSampleText.Size = new Size(390, 21);
             TxtSampleText.TabIndex = 3;
             TxtSampleText.Text = "Hello, C#!";
             // 
@@ -65,11 +89,11 @@
             // 
             ChkItalic.AutoSize = true;
             ChkItalic.Font = new Font("나눔고딕", 8.999999F, FontStyle.Italic);
-            ChkItalic.Location = new Point(367, 37);
+            ChkItalic.Location = new Point(354, 37);
             ChkItalic.Name = "ChkItalic";
             ChkItalic.Size = new Size(59, 18);
             ChkItalic.TabIndex = 2;
-            ChkItalic.Text = "이탤릭";
+            ChkItalic.Text = "기울임";
             ChkItalic.UseVisualStyleBackColor = true;
             ChkItalic.CheckedChanged += ChkItalic_CheckedChanged;
             // 
@@ -77,7 +101,7 @@
             // 
             ChkBold.AutoSize = true;
             ChkBold.Font = new Font("나눔고딕", 8.999999F, FontStyle.Bold);
-            ChkBold.Location = new Point(311, 37);
+            ChkBold.Location = new Point(300, 37);
             ChkBold.Name = "ChkBold";
             ChkBold.Size = new Size(48, 18);
             ChkBold.TabIndex = 2;
@@ -89,7 +113,7 @@
             // 
             CboFonts.Font = new Font("나눔고딕", 8.999999F);
             CboFonts.FormattingEnabled = true;
-            CboFonts.Location = new Point(79, 35);
+            CboFonts.Location = new Point(65, 35);
             CboFonts.Name = "CboFonts";
             CboFonts.Size = new Size(226, 22);
             CboFonts.TabIndex = 1;
@@ -99,23 +123,200 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("나눔고딕", 8.999999F);
-            label1.Location = new Point(34, 38);
+            label1.Location = new Point(23, 38);
             label1.Name = "label1";
-            label1.Size = new Size(39, 14);
+            label1.Size = new Size(36, 14);
             label1.TabIndex = 0;
-            label1.Text = "폰트 : ";
+            label1.Text = "폰트 :";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(PrgDummy);
+            groupBox2.Controls.Add(TrbDummy);
+            groupBox2.Font = new Font("나눔고딕", 8.999999F);
+            groupBox2.Location = new Point(12, 141);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(436, 117);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "트랙바, 프로그레스바";
+            // 
+            // PrgDummy
+            // 
+            PrgDummy.Location = new Point(23, 73);
+            PrgDummy.Maximum = 20;
+            PrgDummy.Name = "PrgDummy";
+            PrgDummy.Size = new Size(390, 25);
+            PrgDummy.TabIndex = 1;
+            // 
+            // TrbDummy
+            // 
+            TrbDummy.Location = new Point(23, 22);
+            TrbDummy.Maximum = 20;
+            TrbDummy.Name = "TrbDummy";
+            TrbDummy.Size = new Size(390, 45);
+            TrbDummy.TabIndex = 0;
+            TrbDummy.Scroll += TrbDummy_Scroll;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(BtnQuestion);
+            groupBox3.Controls.Add(BtnMsgBox);
+            groupBox3.Controls.Add(BtnModaless);
+            groupBox3.Controls.Add(BtnModal);
+            groupBox3.Font = new Font("나눔고딕", 8.999999F);
+            groupBox3.Location = new Point(12, 264);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(436, 72);
+            groupBox3.TabIndex = 2;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "모달창, 모달리스창";
+            // 
+            // BtnQuestion
+            // 
+            BtnQuestion.Location = new Point(356, 22);
+            BtnQuestion.Name = "BtnQuestion";
+            BtnQuestion.Size = new Size(57, 35);
+            BtnQuestion.TabIndex = 1;
+            BtnQuestion.Text = "...";
+            BtnQuestion.UseVisualStyleBackColor = true;
+            BtnQuestion.Click += BtnQuestion_Click;
+            // 
+            // BtnMsgBox
+            // 
+            BtnMsgBox.Location = new Point(245, 22);
+            BtnMsgBox.Name = "BtnMsgBox";
+            BtnMsgBox.Size = new Size(105, 35);
+            BtnMsgBox.TabIndex = 0;
+            BtnMsgBox.Text = "MessageBox";
+            BtnMsgBox.UseVisualStyleBackColor = true;
+            BtnMsgBox.Click += BtnMsgBox_Click;
+            // 
+            // BtnModaless
+            // 
+            BtnModaless.Location = new Point(134, 22);
+            BtnModaless.Name = "BtnModaless";
+            BtnModaless.Size = new Size(105, 35);
+            BtnModaless.TabIndex = 0;
+            BtnModaless.Text = "Modaless";
+            BtnModaless.UseVisualStyleBackColor = true;
+            BtnModaless.Click += BtnModaless_Click;
+            // 
+            // BtnModal
+            // 
+            BtnModal.Location = new Point(23, 22);
+            BtnModal.Name = "BtnModal";
+            BtnModal.Size = new Size(105, 35);
+            BtnModal.TabIndex = 0;
+            BtnModal.Text = "Modal";
+            BtnModal.UseVisualStyleBackColor = true;
+            BtnModal.Click += BtnModal_Click;
+            // 
+            // groupBox4
+            // 
+            groupBox4.Controls.Add(BtnAddChild);
+            groupBox4.Controls.Add(BtnAddRoot);
+            groupBox4.Controls.Add(LsvDummy);
+            groupBox4.Controls.Add(TrvDummy);
+            groupBox4.Font = new Font("나눔고딕", 8.999999F);
+            groupBox4.Location = new Point(12, 342);
+            groupBox4.Name = "groupBox4";
+            groupBox4.Size = new Size(436, 214);
+            groupBox4.TabIndex = 3;
+            groupBox4.TabStop = false;
+            groupBox4.Text = "트리뷰, 리스트뷰";
+            // 
+            // BtnAddChild
+            // 
+            BtnAddChild.Location = new Point(123, 169);
+            BtnAddChild.Name = "BtnAddChild";
+            BtnAddChild.Size = new Size(90, 35);
+            BtnAddChild.TabIndex = 2;
+            BtnAddChild.Text = "자식추가";
+            BtnAddChild.UseVisualStyleBackColor = true;
+            BtnAddChild.Click += BtnAddChild_Click;
+            // 
+            // BtnAddRoot
+            // 
+            BtnAddRoot.Location = new Point(23, 169);
+            BtnAddRoot.Name = "BtnAddRoot";
+            BtnAddRoot.Size = new Size(90, 35);
+            BtnAddRoot.TabIndex = 2;
+            BtnAddRoot.Text = "루트추가";
+            BtnAddRoot.UseVisualStyleBackColor = true;
+            BtnAddRoot.Click += BtnAddRoot_Click;
+            // 
+            // LsvDummy
+            // 
+            LsvDummy.Location = new Point(223, 22);
+            LsvDummy.Name = "LsvDummy";
+            LsvDummy.Size = new Size(190, 141);
+            LsvDummy.TabIndex = 1;
+            LsvDummy.UseCompatibleStateImageBehavior = false;
+            LsvDummy.View = View.Details;
+            // 
+            // TrvDummy
+            // 
+            TrvDummy.Location = new Point(23, 22);
+            TrvDummy.Name = "TrvDummy";
+            TrvDummy.Size = new Size(190, 141);
+            TrvDummy.TabIndex = 0;
+            // 
+            // groupBox5
+            // 
+            groupBox5.Controls.Add(BtnLoad);
+            groupBox5.Controls.Add(PicNormal);
+            groupBox5.Location = new Point(454, 12);
+            groupBox5.Name = "groupBox5";
+            groupBox5.Size = new Size(440, 544);
+            groupBox5.TabIndex = 4;
+            groupBox5.TabStop = false;
+            groupBox5.Text = "픽처박스";
+            // 
+            // BtnLoad
+            // 
+            BtnLoad.Location = new Point(344, 252);
+            BtnLoad.Name = "BtnLoad";
+            BtnLoad.Size = new Size(90, 35);
+            BtnLoad.TabIndex = 1;
+            BtnLoad.Text = "로드";
+            BtnLoad.UseVisualStyleBackColor = true;
+            BtnLoad.Click += BtnLoad_Click;
+            // 
+            // PicNormal
+            // 
+            PicNormal.BackColor = SystemColors.ButtonShadow;
+            PicNormal.Location = new Point(6, 22);
+            PicNormal.Name = "PicNormal";
+            PicNormal.Size = new Size(428, 224);
+            PicNormal.TabIndex = 0;
+            PicNormal.TabStop = false;
+            PicNormal.Click += PicNormal_Click;
             // 
             // FrmMain
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(479, 642);
+            ClientSize = new Size(1184, 571);
+            Controls.Add(groupBox5);
+            Controls.Add(groupBox4);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
             Controls.Add(groupBox1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "FrmMain";
             Text = "컨트롤 예제";
+            FormClosing += FrmMain_FormClosing;
             Load += FrmMain_Load;
             groupBox1.ResumeLayout(false);
             groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)TrbDummy).EndInit();
+            groupBox3.ResumeLayout(false);
+            groupBox4.ResumeLayout(false);
+            groupBox5.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)PicNormal).EndInit();
             ResumeLayout(false);
         }
 
@@ -127,5 +328,22 @@
         private TextBox TxtSampleText;
         private CheckBox ChkItalic;
         private CheckBox ChkBold;
+        private GroupBox groupBox2;
+        private ProgressBar PrgDummy;
+        private TrackBar TrbDummy;
+        private GroupBox groupBox3;
+        private Button BtnMsgBox;
+        private Button BtnModaless;
+        private Button BtnModal;
+        private Button BtnQuestion;
+        private GroupBox groupBox4;
+        private ListView LsvDummy;
+        private TreeView TrvDummy;
+        private Button BtnAddChild;
+        private Button BtnAddRoot;
+        private GroupBox groupBox5;
+        private Button BtnLoad;
+        private PictureBox PicNormal;
+        private OpenFileDialog DlgOpenImage;
     }
 }
--- day04/cs04_winform_app/ex18_winControlApp/FrmMain.cs
+++ day04/cs04_winform_app/ex18_winControlApp/FrmMain.cs
@@ -2,9 +2,13 @@ namespace ex18_winControlApp
 {
     public partial class FrmMain : Form
     {
+        Random rand = new Random(); // 트리뷰 노드이름으로 사용할 랜덤값
         public FrmMain()
         {
-            InitializeComponent();
+            InitializeComponent(); // 디자이너에서 정의한 화면구성 초기화
+
+            LsvDummy.Columns.Add("이름");
+            LsvDummy.Columns.Add("깊이");
         }
 
         private void FrmMain_Load(object sender, EventArgs e)
@@ -32,6 +36,30 @@ namespace ex18_winControlApp
             TxtSampleText.Font = new Font((string)CboFonts.SelectedItem, 12, style);
         }
 
+        void TreeToList()
+        {
+            LsvDummy.Items.Clear();
+            foreach (TreeNode node in TrvDummy.Nodes)
+            {
+                TreeToList(node);
+            }
+        }
+
+        private void TreeToList(TreeNode node)
+        {
+            //throw new NotImplementedException();
+            LsvDummy.Items.Add( // 리스트뷰에 아이템 추가
+                new ListViewItem(
+                    new string[] { node.Text, node.FullPath.Count(f => f == '\\').ToString() }
+                    )
+                );
+
+            foreach (TreeNode subNode in node.Nodes)
+            {
+                TreeToList(subNode);
+            }
+        }
+
         private void CboFonts_SelectedIndexChanged(object sender, EventArgs e)
         {
             ChangeFont();
@@ -45,6 +73,106 @@ namespace ex18_winControlApp
         private void ChkItalic_CheckedChanged(object sender, EventArgs e)
         {
             ChangeFont();
+        }
+
+        /// <summary>
+        /// 트랙바 스크롤 이벤트핸들러
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TrbDummy_Scroll(object sender, EventArgs e)
+        {
+            PrgDummy.Value = TrbDummy.Value; // 트랙바의 포인터를 옮기면 프로그레스바의 값도 같이 변경됨
+        }
+
+        private void BtnModal_Click(object sender, EventArgs e)
+        {
+            Form FrmModal = new Form();
+            FrmModal.Text = "모달창";
+            FrmModal.Width = 300; // 넓이
+            FrmModal.Height = 100; // 높이
+            FrmModal.BackColor = Color.LightCyan; // 배경색
+            FrmModal.ShowDialog();  // 모달창 띄우기
+        } // 자식창이 열려있을 때는 부모창의 작업을 할 수 없음(무조건 자식창을 먼저 닫고 부모창을 닫음)
+
+        private void BtnModaless_Click(object sender, EventArgs e)
+        {
+            Form FrmModaless = new Form();
+            FrmModaless.Text = "모달리스창";
+            FrmModaless.Width = 300; // 넓이
+            FrmModaless.Height = 100; // 높이
+            FrmModaless.BackColor = Color.Lavender; // 배경색
+            FrmModaless.Show();  // 모달리스창 띄우기
+        } // 자식창이 열려있는 상태에서 부모창의 작업이 가능(부모창의 창닫기를 누르면 창이 모두 꺼짐)
+
+        private void BtnMsgBox_Click(object sender, EventArgs e)
+        {
+            // 기본적인 메시지박스 사용법
+            MessageBox.Show(TxtSampleText.Text, "메시지박스", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+
+        private void BtnQuestion_Click(object sender, EventArgs e)
+        {
+            var res = MessageBox.Show("좋아요?", "질문", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (res == DialogResult.Yes)
+            {
+                MessageBox.Show("네 좋아요!");
+            }
+            else if (res == DialogResult.No)
+            {
+                MessageBox.Show("아니요 정말 싫어요!");
+            }
+        }
+
+        // 윈도우 종료버튼을 클릭했을 때 발생하는 이벤트핸들러
+        private void FrmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            var res = MessageBox.Show("정말 종료하시겠습니까?", "종료여부", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (res == DialogResult.No) e.Cancel = true;
+        }
+
+        private void BtnAddRoot_Click(object sender, EventArgs e)
+        {
+            TrvDummy.Nodes.Add(rand.Next().ToString());
+            TreeToList();
+        }
+
+        private void BtnAddChild_Click(object sender, EventArgs e)
+        {
+            if (TrvDummy.SelectedNode == null) // 부모노드를 선택하지 않으면
+            {
+                MessageBox.Show("선택한 노드가 없음", "경고", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return; // 더이상 진행없이 메서드 종료
+            }
+            TrvDummy.SelectedNode.Nodes.Add(rand.Next().ToString());
+            TrvDummy.SelectedNode.Expand();
+            TreeToList();   // 리스트뷰를 다시 그려준다
+        }
+
+        private void BtnLoad_Click(object sender, EventArgs e)
+        {
+            DlgOpenImage.Title = "이미지 열기";
+            // Filter는 확장자를 이미지로만 한정
+            DlgOpenImage.Filter = "Image Files(*.bmp; *.jpg; *.png) | *.bmp; *.jpg; *.png"; // (확장자) | 확장자 한정
+            var res = DlgOpenImage.ShowDialog(this);
+            if (res == DialogResult.OK)
+            {
+                //MessageBox.Show(DlgOpenImage.FileName.ToString());
+                PicNormal.Image = Bitmap.FromFile(DlgOpenImage.FileName);
+            }
+        }
+
+        private void PicNormal_Click(object sender, EventArgs e)
+        {
+            if (PicNormal.SizeMode == PictureBoxSizeMode.Normal)
+            {
+                PicNormal.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            else
+            {
+                PicNormal.SizeMode = PictureBoxSizeMode.Normal;
+            }
         }
     }
 }
